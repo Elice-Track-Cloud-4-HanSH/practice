ad343c33fdef75ec79b440a3367f9336278ebfbe
PromotezCitizen <gkstkdgus821@gmail.com>
2024-07-02 15:04:51 +0900

    메소드 시그니처, static 메소드 사용 실습 추가

---
+++ w02/src/main_static/MainStaticNoneStaticFunc.java
@@ -0,0 +1,15 @@
+package main_static;
+
+public class MainStaticNoneStaticFunc {
+  public int calculate(int a, int b) {
+    return a + b;
+  }
+
+  public static void main(String[] args) {
+    // error: Non-static method 'calculate(int, int)' cannot be referenced from a static contex
+    System.out.println(calculate(10,10));
+
+    // error: 'method_signature.Test.this' cannot be referenced from a static context
+    System.out.println(this.calculate(10,10));
+  }
+}
